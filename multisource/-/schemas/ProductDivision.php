a252d32e6acfed324d6ca72da14be13153d0dd38
fed <platinumaccount@yandex.ru>
2019-02-06 01:03:00 +0300

    \ss\multisource\schemas\ProductDivision

---
+++ multisource/-/schemas/ProductDivision.php
@@ -0,0 +1,20 @@
+<?php namespace ss\multisource\schemas;
+
+class ProductDivision extends \Schema
+{
+    public $table = 'ss_multisource_products_divisions';
+
+    public function blueprint()
+    {
+        return function (\Illuminate\Database\Schema\Blueprint $table) {
+            $table->engine = 'InnoDB';
+
+            $table->increments('id');
+            $table->integer('product_id')->default(0)->unsigned();
+            $table->integer('division_id')->default(0)->unsigned();
+            $table->decimal('price', 14, 2)->nullable();
+
+            // todo alt_price
+        };
+    }
+}
